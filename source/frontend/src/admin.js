26adf2fedd14aba66f87ce0e1507e04197679b1d
StevieDeloof <steven.deloof@student.ehb.be>
2025-06-05 10:17:37 +0200

    De javascript voegt nu in de HTML een link toe naar de stylesheet, in plaats van dat de link direct in de HTML staat

--- source/frontend/src/admin.js
+++ source/frontend/src/admin.js
@@ -9,4 +9,13 @@ document.getElementById('gebruikers').addEventListener('click', () => {
     gebruikers(resultContainer)
 })
 
-dashboard(resultContainer)
+//Add link to stylesheet
+    const head = document.head;
+    //Code borowed from https://stackoverflow.com/questions/11833759/add-stylesheet-to-head-using-javascript-in-body
+    const link = document.createElement('link')
+    link.type = "text/css";
+    link.rel= "stylesheet"
+    link.href = "./src/admin.css"
+    head.appendChild(link)
+    //start with dashboard
+    dashboard(resultContainer)

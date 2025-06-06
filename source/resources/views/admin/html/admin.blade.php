a91c1ec2f4827aab67ce95790665251def31980c
StevieDeloof <145539261+StevieDeloof@users.noreply.github.com>
2025-06-06 19:28:26 +0200

    Gegevens veranderd zodat files juist renderen binnen het framework

--- source/resources/views/admin/html/admin.blade.php
+++ source/resources/views/admin/html/admin.blade.php
@@ -3,11 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/admin/admin.css')
     <title>document</title>
 </head>
 <body>
-    <script src="src/adminVars.js"></script>
-    <script src="src/adminText.js"></script>
-    <script src="src/admin.js"></script>
+    <nav id="navigation">
+        Admin<br>
+        <button class="btn-nav" id="dashboard">Dashboard</button>
+        <button class="btn-nav" id="users">Gebruikers</button>
+        <button class="btn-nav" id="companies">Bedrijven</button>
+        <button class="btn-nav" id="logs">Logs</button>
+    </nav>
+    @vite('resources/js/admin/adminVars.js')
+    @vite('resources/js/admin/adminText.js')
+    @vite('resources/js/admin/admin.js')
 </body>
 </html>

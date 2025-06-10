0eaf3a6eabcebe6677d6c819603874205cbbc182
Arda-exe <aydemirarda10@gmail.com>
2025-06-10 14:52:08 +0200

    Created profile picture in header

--- source/resources/views/student/html/student.blade.php
+++ source/resources/views/student/html/student.blade.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StuDirect</title>
-    @vite ['resources/css/student/student.css', 'resources/js/student/student.js']
+    @vite ('resources/css/student/student.css')
 </head>
 <body>
     <main>
         <section id="sidebar">
             <header>
-                <h1>StuDirect</h1>
+                <div class="header">
+                    <div class="pfp">
+                    </div>
+                    <h1>Geruiker</h1>
+                </div>
                 <nav>
                     <ul>
                         <li><a id="match">Matches</a></li>
@@ -20,10 +24,11 @@
                 </nav>
             </header>
                 <div id="sidebar-content"></div>
-            </section> 
+            </section>
         <section id="screen">
             <div id="screen-content"></div>
         </section>
     </main>
+    @vite ('resources/js/student/student.js')
 </body>
 </html>

695ca6d97127cf267c81cc2cbd765d641b54afe6
Arda-exe <aydemirarda10@gmail.com>
2025-06-11 11:17:15 +0200

    Update student page
    reset everything for new variant implementing blade templates

--- source/resources/views/student/html/student.blade.php
+++ source/resources/views/student/html/student.blade.php
@@ -1,36 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StuDirect</title>
-    @vite ('resources/css/student/student.css')
-</head>
-<body>
-    <main>
-        <section id="sidebar">
-            <header>
-                <div class="header">
-                    <div class="pfp">
-                    </div>
-                    <h1>
-                        {{ \Illuminate\Support\Str::limit($students[0]['first_name'] ?? 'Gebruiker', 20) }}
-                    </h1>
-                </div>
-                <nav>
-                    <ul>
-                        <li><a id="match">Matches</a></li>
-                        <li><a id="message">Berichten</a></li>
-                        <li><a id="calendar">Kalender</a></li>
-                    </ul>
-                </nav>
-            </header>
-                <div id="sidebar-content"></div>
-            </section>
-        <section id="screen">
-            <div id="screen-content"></div>
-        </section>
-    </main>
-    @vite ('resources/js/student/student.js')
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <h2>Section 1</h2>
+    </section>
+    <section class="section">
+        <h2>Section 2</h2>
+    </section>
+    <section class="section">
+        <h2>Section 3</h2>
+    </section>
+@endsection

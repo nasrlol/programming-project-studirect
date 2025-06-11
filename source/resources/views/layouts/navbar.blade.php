d9d951996cbcbc2e7e188709a3512c37d931ac32
Arda-exe <aydemirarda10@gmail.com>
2025-06-11 11:16:32 +0200

    Created blade templates
    created new blade templates for a variant for the student page

---
+++ source/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar">
+    <div class="nav-profile">
+        <div class="profile-pic"></div>
+        <span class="profile-name">{{ $students[0]['first_name'] ?? 'User' }}</span>
+    </div>
+    <ul class="nav-links">
+        <li><a href="" class="active">Home</a></li>
+        <li><a href="">Matches</a></li>
+        <li><a href="">Calendar</a></li>
+    </ul>
+    <div class="nav-logo">
+        <img src="images/studirect.png" alt="StuDirect Logo">
+    </div>
+</nav>

70a0ceef96425664c4060b9c82cd90aa31e4121a
wail zemouri <wail.zemouri@student.ehb.be>
2025-06-13 13:19:52 +0200

    Update web.php

--- source/routes/web.php
+++ source/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
 Route::get('/', [StudentController::class, 'index']);
 
@@ -14,3 +15,12 @@ Route::get('/company', function () {
 });
 
 Route::get('/admin', [AdminController::class, 'showAllUsers']);
+
+/*messages routes*/ 
+Route::get('/messages', [MessageController::class, 'index']);
+Route::get('/messages/{id}', [MessageController::class, 'show']);
+Route::post('/messages', [MessageController::class, 'store']);
+Route::put('/messages/{id}', [MessageController::class, 'update']);
+Route::delete('/messages/{id}', [MessageController::class, 'destroy']);
+
+Route::post('/messages/send', [MessageController::class, 'send']);

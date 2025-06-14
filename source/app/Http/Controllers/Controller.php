ff2e75de6dbb348a6028031926f5465c9db006d3
StevieDeloof <145539261+StevieDeloof@users.noreply.github.com>
2025-06-14 16:14:38 +0200

    Volledige CRUD functionaliteit toegevoegd aan appointments

--- source/app/Http/Controllers/Controller.php
+++ source/app/Http/Controllers/Controller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
 abstract class Controller
 {
-    //
+    // API URLs for appointments, students, and companies
+    protected $appointmentApiUrl = 'http://10.2.160.208/api/appointments';
+    protected string $studentsApiUrl = 'http://10.2.160.208/api/students';
+    protected string $companiesApiUrl = 'http://10.2.160.208/api/companies';
 }

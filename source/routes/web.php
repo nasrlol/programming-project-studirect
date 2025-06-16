db695fc173d7e459c93ef8581b9bd7f73344d063
Arda-exe <aydemirarda10@gmail.com>
2025-06-16 14:05:55 +0200

    Update web.php
    added correct use of messagecontroller in web router

--- source/routes/web.php
+++ source/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\ConnectionController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('welcome');});

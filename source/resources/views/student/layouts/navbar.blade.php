40ae89cc1f34147c8553e87f80ae0300865718cf
Arda-exe <aydemirarda10@gmail.com>
2025-06-17 14:44:02 +0200

    Update navbar.blade.php
    changed component places and added dropdown menu

--- source/resources/views/student/layouts/navbar.blade.php
+++ source/resources/views/student/layouts/navbar.blade.php
@@ -7,8 +7,12 @@
         <li><a href="#" id="matchesBtn">Matches</a></li>
         <li><a href="#" id="calendarBtn">Calendar</a></li>
     </ul>
-    <div class="nav-profile">
+    <div class="nav-profile" id="profileArea">
         <span class="profile-name">{{ $student['first_name'] ?? 'User' }}</span>
         <div class="profile-pic"></div>
+        <div class="profile-dropdown" id="profileDropdown">
+            <a href="#" id="settingsBtn">Settings</a>
+            <a href="#" id="logoutBtn">Log Out</a>
+        </div>
     </div>
 </nav>

7a01ef581c68b031b23a956f90644447282fe557
wail zemouri <wail.zemouri@student.ehb.be>
2025-06-18 04:44:19 +0200

    Update company.blade.php

--- source/resources/views/company/company.blade.php
+++ source/resources/views/company/company.blade.php
@@ -1,34 +1,50 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Main scherm</title>
-    @vite('resources/css/company/company.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company</title>
+    
+    {{-- Vite injectie --}}
+    @vite(['resources/css/company/company.css', 'resources/js/company/company.js'])
 </head>
-
 <body>
-
-    <header id="header">
-        <div class="left">
-            <img id="logo" src='../images/studirect.png' alt="studirect logo">
-        </div>
-        <div class="center"> <button id="homeBtn">home page</button>
-            <button id="messageBtn">messages</button>
-            <button id="calendarBtn">calendar</button>
-        </div>
-        <div class="right">
-            <button id="settingsBtn">settings</button>
-        </div>
+    <header>
+        @include('company.layouts.navbar')
     </header>
 
-    <main id="content">
+  <main id="company-content">
+    @include('company.layouts.home')
+</main>
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const content = document.getElementById("company-content");
 
+            function setActive(tabId) {
+                document.querySelectorAll(".nav-btn").forEach(btn => {
+                    btn.classList.remove("active");
+                });
+                document.getElementById(tabId).classList.add("active");
+            }
 
-    </main>
-     @vite('resources/js/company/company.js')
+     
+            document.getElementById("homeBtn").addEventListener("click", () => {
+                loadView("home");
+                setActive("homeBtn");
+            });
+            document.getElementById("messageBtn").addEventListener("click", () => {
+                loadView("messages");
+                setActive("messageBtn");
+            });
+            document.getElementById("calendarBtn").addEventListener("click", () => {
+                loadView("calendar");
+                setActive("calendarBtn");
+            });
+            document.getElementById("settingsBtn").addEventListener("click", () => {
+                loadView("settings");
+                setActive("settingsBtn");
+            });
+        });
+    </script>
 </body>
-
 </html>

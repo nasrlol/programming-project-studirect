4407244a870434e168595f2560e92c3262661495
Arda-exe <aydemirarda10@gmail.com>
2025-06-18 14:48:22 +0200

    Create welcome.blade.php

---
+++ source/resources/views/company/layouts/welcome.blade.php
@@ -0,0 +1,4 @@
+<div id="notifications">
+    <h2>Welkom op de careerlaunch!</h2>
+    <p>{{ $notification ?? 'Wij zijn verheugd om u te verwelkomen als partner in het begeleiden van de professionals van morgen. Via CareerLaunch krijgt u de kans om uw bedrijf in de kijker te zetten, vacatures te delen en rechtstreeks in contact te komen met gemotiveerde studenten. Samen bouwen we aan de toekomst. Start vandaag nog met het ontdekken van talent!' }}</p>
+</div>

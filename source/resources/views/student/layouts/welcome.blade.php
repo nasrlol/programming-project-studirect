ccfe35ff789326cc06c8c36190520458e9a6c8b1
Arda-exe <aydemirarda10@gmail.com>
2025-06-18 14:57:38 +0200

    renamed notification.blade to welcome.blade

---
+++ source/resources/views/student/layouts/welcome.blade.php
@@ -0,0 +1,9 @@
+<div id="map-fix">
+    <h2>Welcome</h2>
+    <div id="notifications">
+        <p>Welcome to StuDirect! You have access to all the functionalities of the CareerLaunch platform. Start exploring companies and find your perfect internship match.</p>
+    </div>
+    <div id="groundplan">
+        <img src="{{ asset('images/plattegrondEHB.png') }}">
+    </div>
+</div>

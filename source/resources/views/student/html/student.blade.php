3f37fbd5b93c478e95eaba8927db3de4e6c8414f
Arda-exe <aydemirarda10@gmail.com>
2025-06-18 15:13:51 +0200

    Update student.blade.php

--- source/resources/views/student/html/student.blade.php
+++ source/resources/views/student/html/student.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>StuDirect</title>
-    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600&display=swap" rel="stylesheet">
     @vite ('resources/css/student/student.css')
 </head>
 <body>

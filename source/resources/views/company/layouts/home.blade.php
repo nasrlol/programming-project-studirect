58c734f1bf16c35d0f370f8c93ea2f56f2bd8965
wail zemouri <wail.zemouri@student.ehb.be>
2025-06-18 04:44:51 +0200

    Create home.blade.php

---
+++ source/resources/views/company/layouts/home.blade.php
@@ -0,0 +1,12 @@
+
+{{-- resources/views/company/layouts/home.blade.php --}}
+<div class="main-container">
+    <section class="notifications">
+        <h2>Welkom op de careerlaunch!</h2>
+        <p>Wij zijn verheugd om u te verwelkomen als partner in het begeleiden van de professionals van morgen. Via CareerLaunch krijgt u de kans om uw bedrijf in de kijker te zetten, vacatures te delen en rechtstreeks in contact te komen met gemotiveerde studenten. Samen bouwen we aan de toekomst. Start vandaag nog met het ontdekken van talent!</p>
+    </section>
+
+    <section class="map">
+        <img src="{{ asset('images/plattegrondvb.png') }}" alt="venue map">
+    </section>
+</div>

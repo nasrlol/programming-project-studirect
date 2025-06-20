750c991106e25422d370017b20a28d51bc1d1c5f
Arda-exe <aydemirarda10@gmail.com>
2025-06-20 02:07:34 +0200

    Update web.php

--- source/routes/web.php
+++ source/routes/web.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\ConnectionController;
 use App\Http\Controllers\MessageController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('welcome');});
 
